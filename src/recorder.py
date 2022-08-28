af2f3bb9f05c73c19063cae68b98b834af8af760
NoNormalCreeper <2560359315@qq.com>
2022-08-28 17:50:43 +0800

    🔊 contact recorder with logger

--- src/recorder.py
+++ src/recorder.py
@@ -2,6 +2,7 @@ import asyncio
 from websockets import serve
 import ujson as json
 from src.utils import user_config
+from src.log import write_log
 
 
 ws_config = user_config['ws']
@@ -9,13 +10,13 @@ ws_config = user_config['ws']
 async def run(websocket):
     async for message in websocket:
         message = json.loads(message)
-        if message['post_type'] in ['message', 'request', 'notice']:
+        if message['post_type'] == 'message':
             if message['message_type'] == 'private':
-                print(f"{message['user_id']} > {message['raw_message']}")
+                write_log(f"{message['user_id']} > {message['raw_message']}")
             elif message['message_type'] == 'group':
-                print(f"{message['user_id']} in {message['group_id']} > {message['raw_message']}")
+                write_log(f"{message['user_id']} in {message['group_id']} > {message['raw_message']}")
             else:
-                print(f" > {message}")
+                write_log(f" > {message}")
 
 
 async def main():
