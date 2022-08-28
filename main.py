9fcb41c4e1e9c52235ac1af88cb47e6e9dd8e413
NoNormalCreeper <2560359315@qq.com>
2022-08-28 21:36:42 +0800

    ✨ add new cli args

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import sys
 from src.utils import user_config
 from src.log import write_log
 from src.recorder import recorder
-from src.sender import sender
 from src.paser import parser
+
+def main():
+    write_log("Recorder started.")
+    asyncio.run(recorder.run())
 
 if __name__ == "__main__":
     argument_list = sys.argv[1:]
     if argument_list == ['python main.py'][1:]:
-        write_log("Recorder started.")
-        asyncio.run(recorder.run())
+        main()
     else:
-        parser.parse_arg(argument_list)
+        parse_result = parser.parse_arg(argument_list)
+        if parse_result == 'start':
+            main()
--- src/paser.py
+++ src/paser.py
@@ -11,20 +11,31 @@ class Parser:
         user_id = int(args.user) if args.user else -1
         group_id = int(args.group) if args.group else -1
         await sender.send_message(args.message, user_id, group_id)
+    
+    def _stop(self):
+        print("error: this function is still work in progress")
+        quit()
 
     def parse_arg(self, args):
         parser = argparse.ArgumentParser(
             description="QQ Chat Recorder, record your chat history and save them forever.",
             add_help=True,
         )
+
         subparsers = parser.add_subparsers(dest="operation")
         exec_parser = subparsers.add_parser("send", help="Send a message to a user or group.")
         exec_parser.add_argument("-m", "--message", help="Content of message", default=None, required=True)
         exec_parser.add_argument("-u", "--user", help="User ID", default=None, required=False)
         exec_parser.add_argument("-g", "--group", help="Group ID", default=None, required=False)
+        exec_parser = subparsers.add_parser("stop", help="Stop the recorder.")
+        exec_parser = subparsers.add_parser("start", help="Start the recorder.")
         args_namespace = parser.parse_args()
         if args_namespace.operation == "send":
             asyncio.run(self._send(args_namespace))
+        if args_namespace.operation == "start":
+            return 'start'
+        if args_namespace.operation == "stop":
+            self._stop()
 
 
 parser = Parser()
