d9b882ef478d038d2add40deae1c9c6d8ed2cd72
NoNormalCreeper <2560359315@qq.com>
2022-08-28 20:18:07 +0800

    🏗️ use `main.py` to active `recorder.py`

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import asyncio
 from src.utils import user_config
+from src.log import write_log
 from src.recorder import recorder
 
-if "__name__" == "__main__":
-    recorder.run()
+if __name__ == "__main__":
+    write_log("Recorder started.")
+    asyncio.run(recorder.run())
--- src/recorder.py
+++ src/recorder.py
@@ -5,23 +5,26 @@ from src.utils import user_config
 from src.log import write_log
 
 
-ws_config = user_config['ws']
-
-async def run(websocket):
-    async for message in websocket:
-        message = json.loads(message)
-        if message['post_type'] == 'message':
-            if message['message_type'] == 'private':
-                write_log(f"{message['user_id']} > {message['raw_message']}")
-            elif message['message_type'] == 'group':
-                write_log(f"{message['user_id']} in {message['group_id']} > {message['raw_message']}")
-            else:
-                write_log(f" > {message}")
+ws_config = user_config["ws"]
 
 
-async def main():
-    async with serve(run, ws_config['host'], ws_config['port']):
-        await asyncio.Future()  # run forever
+class Recorder:
+    async def _record(self, websocket):
+        async for message in websocket:
+            message = json.loads(message)
+            if message["post_type"] == "message":
+                if message["message_type"] == "private":
+                    write_log(f"{message['user_id']} > {message['raw_message']}")
+                elif message["message_type"] == "group":
+                    write_log(
+                        f"{message['user_id']} in {message['group_id']} > {message['raw_message']}"
+                    )
+                else:
+                    write_log(f" > {message}")
+
+    async def run(self):
+        async with serve(self._record, ws_config["host"], ws_config["port"]):
+            await asyncio.Future()  # run forever
 
 
-asyncio.run(main())
+recorder = Recorder()
