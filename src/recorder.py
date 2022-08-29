22a2d8f332307340edf6a92ab7e85082d21f244a
NoNormalCreeper <2560359315@qq.com>
2022-08-29 22:07:42 +0800

    ✨ record message sent by myself

--- src/recorder.py
+++ src/recorder.py
@@ -12,16 +12,22 @@ class Recorder:
     async def _record(self, websocket):
         async for message in websocket:
             message = json.loads(message)
-            if message["post_type"] == "message":
+            if message["post_type"] in ("message", "message_sent"):
+                message['raw_message'].replace('\n', '┙').replace('\r', '┙')
+                if message["post_type"] == "message_sent":
+                    message['user_id'] = "ME"
                 if message["message_type"] != "guild":
                     if message["message_type"] == "private":
-                        write_log(f"{message['user_id']} > {message['raw_message']}")
+                        if message["user_id"] == "ME":
+                            write_log(f"{message['target_id']} < {message['raw_message']}")
+                        else:
+                            write_log(f"{message['user_id']} > {message['raw_message']}")
                     elif message["message_type"] == "group":
                         write_log(
                             f"{message['user_id']} in {message['group_id']} > {message['raw_message']}"
                         )
                     else:
-                        write_log(f"Unknown type of message > {message}")
+                        write_log(f"Unknown type of message > {json.dumps(message, sort_keys=True, indent=4)}")
 
     async def run(self):
         async with serve(self._record, ws_config["host"], ws_config["port"]):
