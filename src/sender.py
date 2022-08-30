e3e00d71a46dc8d51a9eccfb4a5b6356876b148b
NoNormalCreeper <2560359315@qq.com>
2022-08-30 12:04:42 +0800

    💄 beautify json output in `stdout`

--- src/sender.py
+++ src/sender.py
@@ -1,6 +1,7 @@
 import asyncio
 import websockets
 import ujson as json
+from pygments import highlight, lexers, formatters
 from .utils import user_config
 from .log import write_log
 import time
@@ -9,19 +10,27 @@ import time
 ws_config = user_config["ws"]
 
 
-def _get_params(message, user_id: int = -1, group_id: int = -1) -> dict:
-    '''
-    Get params for sending message.
-    '''
-    params = {"message": message}
-    # group_id is more important than user_id
-    if group_id != -1:
-        params["group_id"] = group_id
-    elif user_id != -1:
-        params["user_id"] = user_id
-    return params
+def beautify_json(json_data) -> str:
+    if isinstance(json_data, str):
+        json_data = json.loads(json_data)
+    json_data = json.dumps(json_data, sort_keys=True, indent=4, ensure_ascii=False)
+    json_data = highlight(json_data, lexers.JsonLexer(), formatters.TerminalFormatter())
+    return json_data
+
 
 class Sender:
+    def _get_params(self, message, user_id: int = -1, group_id: int = -1) -> dict:
+        '''
+        Get params for sending message.
+        '''
+        params = {"message": message}
+        # group_id is more important than user_id
+        if group_id != -1:
+            params["group_id"] = group_id
+        elif user_id != -1:
+            params["user_id"] = user_id
+        return params
+    
     async def _call_api(self, action: str, params: dict, echo_message: str = "call_mannually", timeout: int = 30, print_response: bool = True) -> dict:
         start_time = time.time()
         echo_message += f"_{start_time}"
@@ -41,9 +50,8 @@ class Sender:
                     break
                 if time.time() - start_time > timeout:
                     raise TimeoutError(f"Timeout when calling {action}")
-            response_str = json.dumps(response, sort_keys=True, indent=4)
             if print_response:
-                print(f"\033[1;36mResponse <\033[0m \n{response_str}")
+                print(f"\033[1;37mResponse <\033[0m \n{beautify_json(response)}")
             return response
 
     async def call_api(self, action: str, params: dict) -> None:
@@ -51,7 +59,7 @@ class Sender:
     
     async def send_message(self, message, user_id: int = -1, group_id: int = -1) -> None:
         # Why to set default value to -1? To keep the type of user_id and group_id are int.
-        await self._call_api('send_msg', _get_params(message, user_id, group_id), "send_mannuall_by_cmd")
+        await self._call_api('send_msg', self._get_params(message, user_id, group_id), "send_mannuall_by_cmd")
 
 
 sender = Sender()
